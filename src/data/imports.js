d8a757ce76e824ec845a2714d55725314240c133
SkLz <isklzcode@gmail.com>
2021-04-28 14:53:36 +0100

    Improved node importer
    * Changed the importer to run on jQuery ready
    + Added proper flags for using default export and script tags
    + Added global scope importing function

--- src/data/imports.js
+++ src/data/imports.js
@@ -1,23 +1,46 @@
 (function(libs) {
-    console.log("Node importer starting up");
-
     const path = require("path");
-    var list = document.getElementsByTagName("script");
+    const $ = require("jquery");
 
-    for (let i = 0; i < list.length; i++) {
-        const elem = list[i];
+    /**
+     * Imports a library available in the webs/libs folder
+     * @param {string} lib Path to the library relative to web/libs
+     * @param {boolean} defaultExport Whether to use the default export of the script
+     * @param {boolean} global Whether to import the script in the global scope
+     */
+    window.importLib = (lib, defaultExport = true, global = false) => {
+        const filePath = path.join(libs, lib);
 
-        const libPath = elem.getAttribute("src");
-        const name = elem.getAttribute("as");
-
-        if (elem.hasAttribute("lib") && libPath != null) {
-            const filePath = path.join(libs, libPath);
-            console.log("Importing file " + filePath + ((name != null) ? "as " + name : ""));
-
-            if (name != null) window[name] = require(filePath);
-            else require(filePath);
-
-            elem.remove();
+        if (global) {
+            let script = document.createElement("script");
+            script.src = filePath;
+            document.head.appendChild(script);
+            return;
         }
+
+        return defaultExport ? require(filePath).default : require(filePath);
     }
+
+    $.ready.then(() => {
+        var list = document.querySelectorAll("script");
+
+        for (let i = 0; i < list.length; i++) {
+            const elem = list[i];
+
+            const libPath = elem.getAttribute("src");
+            const name = elem.getAttribute("as");
+
+            const flags = elem.getAttribute("data");
+            const def = flags == null || flags.indexOf("default") != -1;
+            const global = flags != null && flags.indexOf("global") != -1;
+
+            // Lib elements with a specified path
+            if (elem.hasAttribute("lib") && libPath != null) {
+                if (name != null) window[name] = window.importLib(libPath, def, global);
+                else window.importLib(libPath, def, global);
+
+                elem.remove();
+            }
+        }
+    });
 })
