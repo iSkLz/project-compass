076f285a9b321159084b788c84ac3371c9ff3f63
SkLz <isklzcode@gmail.com>
2021-05-01 16:46:04 +0100

    Updated boot file
    + All UIWindow instances are now destroyed when all windows are closed

--- src/core/boot.ts
+++ src/core/boot.ts
@@ -2,25 +2,39 @@ import Core from "./core.js";
 import { app } from "electron";
 import { serializeError } from "serialize-error";
 import os from "../helpers/os.js";
+import UIWindow from "./ui/ui.js";
 
 // TODO: Tray
 
+function logException(ex: Error) {
+    // TODO: Log more useful information for known errors
+
+    // List of known errors:
+    // - Bad JSON (mostly in IPC handlers)
+
+    // TODO: Log error when the logger is finished
+    var details = serializeError(ex);
+
+    console.log(JSON.stringify(details));
+
+    app.exit(-1);
+}
+
 app.on("window-all-closed", () => {
-    if (!os.isMac) app.quit();
-})
+    if (!os.isMac) {
+        for (const [ID, win] of UIWindow.Instances) {
+            win.destroy();
+        }
+        app.quit();
+    }
+});
 
 app.once("ready", () => {
     try {
         new Core().init();
     } catch (ex) {
-        // TODO: Log more useful information for known errors
-
-        // List of known errors:
-        // - Bad JSON (mostly in IPC handlers)
-
-        // TODO: Log error when the logger is finished
-        var details = serializeError(ex);
-
-        app.exit(-1);
+        logException(ex);
     }
 });
+
+//process.on("uncaughtException", logException);
